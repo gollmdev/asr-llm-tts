58d1064ad1ea8d92777bb207a74584032be0177b
wangyang <1749748955@qq.com>
2026-03-04 16:00:12 +0800

    update dag

--- ai/dag/dag.go
+++ ai/dag/dag.go
@@ -99,7 +99,7 @@ func NewEngine(ctx context.Context, cancel context.CancelFunc, dag *DAG) *Engine
 	return e
 }
 
-func (e *Engine) Start() error {
+func (e *Engine) Start() {
 	// g, ctx := errgroup.WithContext(e.ctx)
 
 	// ⭐ 预启动 AlwaysOn 节点
@@ -117,11 +117,27 @@ func (e *Engine) Start() error {
 		e.wg.Wait()
 		// e.cancel() // 任何节点出错或完成都取消整个引擎
 		close(e.bus)
+
+		// delete nodeInput nodeState
+		for id := range e.nodeInput {
+			// close(e.nodeInput[id])
+			delete(e.nodeInput, id)
+			// delete(e.nodeStates, id)
+		}
+		for id := range e.nodeStates {
+			delete(e.nodeStates, id)
+		}
+
 		log.Printf("Engine stopped!")
 		// return err
 		return nil
 	})
-	return e.g.Wait()
+}
+func (e *Engine) Close() {
+	if err := e.g.Wait(); err != nil {
+		log.Println("close error:", err)
+	}
+	e.cancel()
 }
 
 func (e *Engine) dispatchLoop() error {
@@ -158,8 +174,8 @@ func (e *Engine) startNode(id string) {
 	// state.activeUpstreams++
 	e.wg.Add(1)
 	e.g.Go(func() error {
-		// log.Printf(">>>> start %s", id)
-		// defer log.Printf(">>>> end %s", id)
+		log.Printf(">>>> start node  %s", id)
+		defer log.Printf(">>>> end node %s", id)
 
 		// defer e.wg.Done()
 		// state.mu.Lock()
@@ -167,11 +183,11 @@ func (e *Engine) startNode(id string) {
 		// // state.activeUpstreams++
 		// state.mu.Unlock()
 
-		e.bus <- Event{
-			Type: "node_start",
-			From: id,
-			Data: nil,
-		}
+		// e.bus <- Event{
+		// 	Type: "node_start",
+		// 	From: id,
+		// 	Data: nil,
+		// }
 		err := e.dag.Nodes[id].Run(
 			e.ctx,
 			e.nodeInput[id],
@@ -188,24 +204,10 @@ func (e *Engine) startNode(id string) {
 		state.mu.Lock()
 		state.done = true
 		state.mu.Unlock()
-		// log.Printf("onNodeExit close %s", id)
 
 		return err
 	})
 
-	// e.waitOnce.Do(func() {
-	// 	e.g.Go(func() error {
-	// 		e.wg.Wait()
-	// 		e.cancel() // 任何节点出错或完成都取消整个引擎
-	// 		// close(e.bus)
-	// 		log.Printf("Engine stopped!")
-	// 		// return err
-	// 		return nil
-	// 	})
-	// })
-	// go func() {
-
-	// }()
 }
 
 // func (e *Engine) onNodeExit(id string) {
@@ -229,7 +231,12 @@ func (e *Engine) startNode(id string) {
 // 可能 dispatch 又 startNode
 // 导致 wg.Wait() 提前返回或语义失效
 func (e *Engine) dispatch(ev Event) {
-
+	if ev.Type == "node_done" {
+		e.wg.Done()
+		log.Printf(">>>> end signal %s", ev.From)
+		e.handleUpstreamDone(ev.From)
+		return
+	}
 	for _, edge := range e.dag.Edges {
 
 		if edge.FromNode != ev.From || edge.OnEvent != ev.Type {
@@ -239,41 +246,35 @@ func (e *Engine) dispatch(ev Event) {
 
 		node := e.dag.Nodes[target]
 
-		// ⭐ Lazy 节点才按需启动
+		// 开启延迟加载的 toNode 节点的 goroutine
+		// {FromNode: "answer", OnEvent: "llm_chunk", ToNode: "tts"}
+		// engine.nodeInput["answer"] <- Event{
+		// 	Type: "user_input",
+		// 	From: "external",
+		// 	Data: "Tell me about Golang",
+		// }
+		// 例如首次向 answer 发送消息 tts 是延迟加载
 		if node.Mode() == ModeLazy {
 			e.startNode(target)
 		}
 
-		// if ev.Type == "node_start" {
+		// activeUpstreams 应该表示 “有多少个上游节点正在向我发送数据”，而不是收到了多少个事件
+		// 如果 toNode 的上游fromNode已经被激活，则不再增加 activeUpstreams
+		// 后续在fromNode结束时，通过edge找到toNode，对 activeUpstreams 进行相应的减少
 
-		// 	return
-
-		// }
 		state := e.nodeStates[target]
-		log.Printf(">>>> start %s", target)
-
 		state.mu.Lock()
 		if !state.upstreamActive[ev.From] {
 			state.activeUpstreams++
 			state.upstreamActive[ev.From] = true
-
 		}
 		state.mu.Unlock()
-		// activeUpstreams 应该表示 “有多少个上游节点正在向我发送数据”，而不是收到了多少个事件
-		// 如果 toNode 的上游fromNode已经被激活，则不再增加 activeUpstreams
-		// 后续在fromNode结束时，通过edge找到toNode，对 activeUpstreams 进行相应的减少
 
 		e.nodeInput[target] <- ev
 
 		log.Printf("from  %s to %s receive %s", edge.FromNode, target, ev.Data)
 	}
-	if ev.Type == "node_done" {
-		e.wg.Done()
-		log.Printf(">>>> end done %s", ev.From)
-		e.handleUpstreamDone(ev.From)
-		// e.onNodeExit(ev.From)
-		// close(e.nodeInput[ev.From])
-	}
+
 }
 
 func (e *Engine) handleUpstreamDone(from string) {
@@ -288,43 +289,24 @@ func (e *Engine) handleUpstreamDone(from string) {
 		state := e.nodeStates[target]
 
 		state.mu.Lock()
-		// state.upstreams--
 
-		// if state.upstreams == 0 && !state.done {
-		// 	close(e.nodeInput[target])
-		// 	log.Printf("node done: %s", target)
-
-		// }
-		// state.activeUpstreams--
 		if state.upstreamActive[from] {
 			delete(state.upstreamActive, from)
 			state.activeUpstreams--
-			log.Printf("Node %s activeUpstreams: %d", target, state.activeUpstreams)
+			// log.Printf("Node %s activeUpstreams: %d", target, state.activeUpstreams)
 		}
 
 		//  && state.started
 		if state.activeUpstreams == 0 && !state.done {
 			close(e.nodeInput[target])
+			// delete(e.nodeStates, target)
+			// delete(e.nodeInput, target)
+
 			log.Printf("node done: %s", target)
 		}
 		state.mu.Unlock()
 	}
 
-	// for _, node := range e.dag.Nodes {
-	// 	state := e.nodeStates[node.ID()]
-	// 	// state.mu.Lock()
-	// 	if state.activeUpstreams != 0 {
-	// 		return
-	// 	}
-	// }
-	// // close(e.bus)
-	// log.Println(e.dag.Nodes)
-	// print all node activeUpstreams
-	// for _, node := range e.dag.Nodes {
-	// 	state := e.nodeStates[node.ID()]
-	// 	log.Printf("Node %s activeUpstreams: %d", node.ID(), state.activeUpstreams)
-	// }
-	// log.Println("Engine stopped!")
 }
 
 func Test() {
@@ -359,5 +341,5 @@ func Test() {
 		}
 	}()
 	engine.Start()
-
+	engine.Close()
 }
